13cdbc4da1ce3c3edf59f349f06b283bbe4461a4
juanjov <juanjov@abe5b35d-8142-0410-8ea1-df6b371289ac>
2008-02-19 04:21:15 +0000

    Agrego la clase Tabla.

--- Host.cpp
+++ Host.cpp
@@ -1,78 +1,76 @@
 #include "Classes.h"
 
-public class Host : public I_Intercambiador, private I_Generador
+public class Host
 {
 
-private:
-	IP m_IP;
-	int m_iRandomNumber;
+	private:
+		IP m_IP;
+		int m_iRandomNumber;
 	
-	ofstream m_ArchivoSalida;
+		ofstream m_ArchivoSalida;
 
-	Router* m_Gateway;
+		Router* m_Gateway;
 
-	IP generarDestino()
-	{
-		calcularRandom(admin.getCantRouters());
+		IP generarDestino()
+		{
+			m_iRandomNumber = calcularRandom(admin.getCantRouters());
 
-		IP ip(m_iRandomNumber, calcularRandom(admin.getCantHosts(m_iRandomNumber)));
+			IP ip(m_iRandomNumber, calcularRandom(admin.getCantHosts(m_iRandomNumber)));
+	
+			return ip;
+		}
 
-		return ip;
-	}
-
-	Pagina generarPagina()
-	{		
+		Pagina generarPagina()
+		{		
+			
+			Pagina pagina(calcularRandom(100), generarDestino(), m_IP);
 		
-		Pagina pagina(calcularRandom(100), generarDestino(), m_IP);
+			return pagina;
+		}	
+	
+		int calcularRandom(int iLim)
+		{	
+			return ((int) (rand() * iLim)) + 1;
+		}	
 
-		return pagina;
-	}	
-
-	int calcularRandom(int ilim)
-	{
-		m_iRandomNumber = ((int) (rand() * ilim)) + 1;
-
-		return m_iRandomNumber;
-	}	
-
-public:
-	Host(int i1Oct, int i2Oct)
-	{
-		setIP(i1Oct, i2Oct);
-
-		string szAuxString = "Host_" + m_IP.toString() + ".log";
-		m_ArchivoSalida.open(szAuxString);
+	public:
+		Host(int i1Oct, int i2Oct)
+		{
+			setIP(i1Oct, i2Oct);
+	
+			string szAuxString = "Host_" + m_IP.toString() + ".log";
+			m_ArchivoSalida.open(szAuxString);
 		
-		srand((unsigned) time(0));
+			srand((unsigned) time(0));
+	
+			m_Gateway = admin.getGateway(this, i1Oct);
+		}
 
-		m_Gateway = admin.getGateway(this, i1Oct);
-	}
+		void setIP(int i1Oct, int i2Oct)
+		{
+			getIP().setPrimerOcteto(i1Oct);
+			getIP().setSegundoOcteto(i2Oct);
+		}
 
-	void setIP(int i1Oct, int i2Oct)
-	{
-		getIP().setPrimerOcteto(i1Oct);
-		getIP().setSegundoOcteto(i2Oct);
-	}
+		IP getIP()
+		{
+			return m_IP;
+		}	
 
-	IP getIP()
-	{
-		return m_IP;
-	}
+		void enviar()
+		{
+			Pagina pagina;
+			pagina = generarPagina();
+	
+			m_Gateway->(recibir(pagina));
 
-	void enviar()
-	{
-		Pagina pagina;
-		pagina = generarPagina();
-
-		m_Gateway->(recibir(pagina));
-
-		for (int cii = 0; cii < pagina.getCantPaquetes(); cii++)
-			m_ArchivoSalida << "Sale_ " + pagina.getPaquete(cii).toString() << endl;		
-	}
-
-	void recibir(Pagina pagina)
-	{
-		for (int cii = 0; cii < pagina.getCantPaquetes(); cii++)
-			m_ArchivoSalida << "Entra_ " + pagina.getPaquete(cii).toString() << endl;			
-	}
+			for (int cii = 0; cii < pagina.getCantPaquetes(); cii++)
+				m_ArchivoSalida << "Sale_ " + pagina.getPaquete(cii).toString() << endl;		
+		}
+	
+		void recibir(Pagina pagina)
+		{
+			for (int cii = 0; cii < pagina.getCantPaquetes(); cii++)
+				m_ArchivoSalida << "Entra_ " + pagina.getPaquete(cii).toString() << endl;			
+		}
 };
--- Pagina.cpp
+++ Pagina.cpp
@@ -1,62 +1,61 @@
-#include "Classes.h"
+#include <cstdlib>
 
 public class Pagina
 {
 
-private:
-	list<Paquete> m_ListaPaquetes;
-	int m_iCantPaquetes;
-	IP m_IPDestino;
-	IP m_IPOrigen;
+	private:
+		list<Paquete> m_ListaPaquetes;
+		int m_iCantPaquetes;
+		IP m_IPDestino;
+		IP m_IPOrigen;
+		float m_fIDPagina;
 
-	float m_fIDPagina;
+	public:
+		Pagina(int iCantPaquetes, IP ipDestino, IP ipOrigen)
+		{
+			srand((unsigned) time(0));
 
-public:
-	Pagina(int iCantPaquetes, IP ipDestino, IP ipOrigen)
-	{
-		srand((unsigned) time(0));
+			m_fIDPagina = rand();
 
-		m_fIDPagina = rand();
-		
-		m_iCantPaquetes = iCantPaquetes;
-		m_IPDestino = ipDestino;
-		m_IPOrigen = ipOrigen;
-		
-		for (int cii = 0; cii < iCantPaquetes; cii++) {
-			Paquete paquete(ipDestino, ipOrigen, m_fIDPagina, (cii + 1));
-			m_ListaPaquetes.push_back(paquete);
+			m_iCantPaquetes = iCantPaquetes;
+			m_IPDestino = ipDestino;
+			m_IPOrigen = ipOrigen;
+
+			for (int cii = 0; cii < iCantPaquetes; cii++) {
+				Paquete paquete(ipDestino, ipOrigen, m_fIDPagina, (cii + 1));
+				m_ListaPaquetes.push_back(paquete);
+			}
 		}
-	}
 
-	Pagina(list<Paquete> listaPaquetes)
-	{
-		m_ListaPaquetes = listaPaquetes;
-		m_iCantPaquetes = listaPaquetes.size();
+		Pagina(list<Paquete> listaPaquetes)
+		{
+			m_ListaPaquetes = listaPaquetes;
+			m_iCantPaquetes = listaPaquetes.size();
 
-		list<Paquete>::iterator iiterator = listaPaquetes.begin();
-		m_fIDPagina = (*iiterator).getIDPagina();
-		m_IPDestino = (*iiterator).getIPDestino();
-		m_IPOrigen = (*iiterator).getIPOrigen();
-	}
+			list<Paquete>::iterator iiterator = listaPaquetes.begin();
+			m_fIDPagina = (*iiterator).getIDPagina();
+			m_IPDestino = (*iiterator).getIPDestino();
+			m_IPOrigen = (*iiterator).getIPOrigen();
+		}
 
-	int getCantPaquetes()
-	{
-		return m_iCantPaquetes;
-	}
+		int getCantPaquetes()
+		{
+			return m_iCantPaquetes;
+		}
 
-	IP getIPDestino()
-	{
-		return m_IPDestino;
-	}
+		IP getIPDestino()
+		{
+			return m_IPDestino;
+		}
 
-	Paquete getPaquete(int ii)
-	{
-		list<Paquete>::iterator it = m_ListaPaquetes.begin();
+		Paquete getPaquete(int ii)
+		{
+			list<Paquete>::iterator it = m_ListaPaquetes.begin();
 
-		for (int cii = 0; cii < (ii - 1); cii++)
-			it++;
+			for (int cii = 0; cii < (ii - 1); cii++)
+				it++;
 
-		return (*it);
-	}
+			return (*it);
+		}
 };
 
